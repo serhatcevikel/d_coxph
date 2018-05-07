d5f233321a7c77172ff13ac855a606c99d319dfe
Melle Sieswerda <m.sieswerda@iknl.nl>
2018-05-07 22:41:04 +0200

    Script to build docker now pushes to docker-registry.distributedlearning.online

--- build_docker.sh
+++ build_docker.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
-HOST=localhost
-PORT=5001
+
+# HOST=localhost
+# PORT=5000
+
+HOST=docker-registry.distributedlearning.online
+PORT=443
+
 IMAGE=dl_coxph
 
-docker build -t $IMAGE -t $HOST:$PORT/$IMAGE .
-docker push $HOST:$PORT/$IMAGE
+if [ $PORT -eq 443 ]
+then
+    docker build -t $IMAGE -t $HOST/$IMAGE .
+    docker push $HOST/$IMAGE
+else
+    docker build -t $IMAGE -t $HOST:$PORT/$IMAGE .
+    docker push $HOST:$PORT/$IMAGE
+fi
+
+
