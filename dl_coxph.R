0567ed95776df81e18697499505af5370f522bc9
Melle Sieswerda <melle@sieswerda.net>
2018-07-23 10:22:13 +0200

    Fix for issue with uneven distribution of event times.

--- dl_coxph.R
+++ dl_coxph.R
@@ -138,11 +138,11 @@ RPC_perform_iteration <- function(df, expl_vars, time_col, censor_col, beta, uni
     m <- length(expl_vars)
 
     # initialize matrices for the aggregates we're about to compute
-    agg1 <- array(dim=c(D))
-    agg2 <- array(dim=c(D, m))
+    agg1 <- array(dim=c(D), 0)
+    agg2 <- array(dim=c(D, m), 0)
     dimnames(agg2) <- list(NULL, expl_vars)
 
-    agg3 <- array(dim=c(D, m, m))
+    agg3 <- array(dim=c(D, m, m), 0)
     dimnames(agg3) <- list(NULL, expl_vars, expl_vars)
 
     for (i in 1:D) {
@@ -150,6 +150,10 @@ RPC_perform_iteration <- function(df, expl_vars, time_col, censor_col, beta, uni
       # Compute the risk set at time t; this includes *all* patients that have a
       # survival time greater than or equal to the current time
       R_i <- as.matrix(data$Z[data$time >= unique_event_times[i], ])
+
+      if (nrow(R_i) == 0) {
+        break
+      }
 
       # aggregate 1: SUM_risk[exp(beta * z)]
       ebz <- exp(R_i %*% beta)
@@ -442,7 +446,7 @@ call <- function(client, method, ...) {
   # collaboration, results, etc. the entry "results" is itself a list with
   # one entry for each site. The site's actual result is contained in the
   # named list member 'result' and is encoded using saveRDS.
-  sites <- result_dict$results
+  sites <- result_dict$rhedesults
   results <- list()
 
   for (k in 1:length(sites)) {
